69fb412d002a1c615884dc2e8ab02affb895bb3e
Fernando LB <103178820+FernandoLLB@users.noreply.github.com>
2026-02-10 10:12:07 +0100

    Cambios UTF

--- src/main/java/es/fempa/acd/demosecurityproductos/config/LocaleConfig.java
+++ src/main/java/es/fempa/acd/demosecurityproductos/config/LocaleConfig.java
@@ -2,12 +2,16 @@ package es.fempa.acd.demosecurityproductos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -30,5 +34,12 @@ public class LocaleConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        stringConverter.setWriteAcceptCharset(false);
+        converters.add(stringConverter);
     }
 }
